0dd4954cf5abd22ab4a38889074515369b36f48c
Martin Thomsen <1078136@ucn.dk>
2021-04-30 11:13:28 +0200

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// hejsa
+// hejsa hello
 
 @NgModule({
   declarations: [
